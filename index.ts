1a7f35abcf9fc1d558f4aef52b9f4e838c009f52
Artit Kiuwilai <first087@gmail.com>
2026-06-03 15:12:43 +0700

    feat: setup commander and cli entry point

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+import { Command } from 'commander';
+import packageJson from './package.json' with { type: 'json' };
+
+const program = new Command();
+
+program
+  .name('agys')
+  .description('A CLI tool for managing antigravity-cli oauth tokens')
+  .version(packageJson.version);
+
+program.parse(process.argv);
